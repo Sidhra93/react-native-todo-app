25d10f0e0fd0c77818f97a41ec3f7afc68983f1c
Sidhra Cader <scader@mel0bhmac559.ap.corp.jwt.com>
2020-02-04 11:24:33 +1100

    style and rendering list

--- App.js
+++ App.js
@@ -1,19 +1,40 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+    const [ todos, setTodos ] = useState([
+        { text: 'buy coffee', key: '1' },
+        { text: 'create an app', key: '2' },
+        { text: 'do laundry', key: '3' }
+    ])
+
+    return (
+        <View style={styles.container}>
+            {/* header */}
+            <View style={styles.content}>
+                {/* todo form */}
+                <View style={styles.list}>
+                    <FlatList
+                        data={ todos }
+                        renderItem={ ({item}) => (
+                            <Text>{ item.text }</Text>
+                        ) }
+                    />
+                </View>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    content: {
+        padding: 40
+    },
+    list: {
+        marginTop: 20
+    }
 });
