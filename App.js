f79b6e9831ad23357726e41a07a4118e18541f28
Sidhra Cader <scader@mel0bhmac559.ap.corp.jwt.com>
2020-02-04 13:38:16 +1100

    Add form component

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Header from './components/Header'
 import TodoItem from './components/TodoItem'
+import AddTodo from './components/AddTodo'
 
 export default function App() {
     const [ todos, setTodos ] = useState([
@@ -9,6 +10,16 @@ export default function App() {
         { text: 'clean the house', key: '2' },
         { text: 'do laundry', key: '3' }
     ])
+
+    const submitHandler = (text) => {
+        setTodos((prevTodos) => {
+            return [
+                { text: text, key: Math.random().toString() },
+                ...prevTodos
+            ]
+        })
+        
+    }
 
     const pressHandler = (key) => {
         setTodos((prevTodos) => {
@@ -20,7 +31,7 @@ export default function App() {
         <View style={styles.container}>
             <Header />
             <View style={styles.content}>
-                {/* todo form */}
+                <AddTodo submitHandler={ submitHandler } />
                 <View style={styles.list}>
                     <FlatList
                         data={ todos }
